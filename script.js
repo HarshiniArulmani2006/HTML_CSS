1331114c2bcbc201ea798b1004d0d37931da204c
A.HARSHINI <harshiniat2006@gmail.com>
2025-12-04 08:50:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+console.log("Hello HARSHINI");
+var a=10;
+console.log("The value of a is : "+a);
+var a=20;
+console.log("The new value of a is: "+a);
+var a=30;
+console.log("The updated value of a is: "+a);
+let b=15;
+console.log("The value of b is: "+b);
+const d=10;
+console.log("The constant value is: "+d);
+let name="Harshini";
+let age=22;
+let issafe=true;
+let x;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof issafe);
+console.log(typeof x);
+
+let num=10;
+let num1=10;
+let sum=num+num1;
+console.log("The sum is: ",sum);
+
+//arrow function 
+const addition=(a,b)=>{
+    return a+b;
+}
+let result=addition(3,4);
+console.log("The addition is:",result);
